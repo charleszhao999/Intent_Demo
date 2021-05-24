a7569db518e26915c5c94231565a6539e8983a6d
charleszhao999 <charleszhao999@163.com>
2021-05-24 15:13:25 +0800

    Initial commit

---
+++ app/src/main/java/cn/jan29th/intent/Activity2.java
@@ -0,0 +1,14 @@
+package cn.jan29th.intent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
